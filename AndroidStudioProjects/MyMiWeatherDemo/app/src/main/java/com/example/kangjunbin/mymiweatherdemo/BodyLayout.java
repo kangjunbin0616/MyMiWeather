a0a1e555630d2ebb19c71525a7727b0a17bc02fc
kangjunbin <kangjunbin@kuaishou.com>
2020-07-20 21:14:50 +0800

    z最新

---
+++ AndroidStudioProjects/MyMiWeatherDemo/app/src/main/java/com/example/kangjunbin/mymiweatherdemo/BodyLayout.java
@@ -0,0 +1,19 @@
+package com.example.kangjunbin.mymiweatherdemo;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import java.util.jar.Attributes;
+
+/**
+ * Created by kangjunbin on 2020/7/20.
+ */
+
+public class BodyLayout extends LinearLayout {
+    public BodyLayout(Context context, AttributeSet attributeSet){
+        super(context,attributeSet);
+        LayoutInflater.from(context).inflate(android.R.layout.body_layout,this);
+    }
+}
